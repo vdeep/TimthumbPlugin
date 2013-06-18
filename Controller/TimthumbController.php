8a58e5cb4e468281027ad4b70e8aacf90160c0e0
Vishal Deep <vishal@logicielsolutions.co.in>
2013-06-18 11:55:50 +0530

    Attached Timthumb debug with app's debug mode

--- Controller/TimthumbController.php
+++ Controller/TimthumbController.php
@@ -6,7 +6,11 @@ class TimthumbController extends TimthumbAppController {
     public function image() {
         $this->autoRender = false;
         $this->layout = null;
-        define('DEBUG_ON', true);
+        if ( Configure::read('debug') !== 0 ) {
+            define('DEBUG_ON', true);
+        } else {
+            define('DEBUG_ON', false);
+        }
         define('FILE_CACHE_DIRECTORY', Configure::read('TimthumbCacheDir'));
         App::import('Vendor', 'Timthumb.timthumb');
     }
