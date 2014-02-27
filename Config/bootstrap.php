4072c87d73198a4f297d3a3d0afc21389729d173
Dan Voyce <voycey@users.noreply.github.com>
2014-02-27 16:25:58 +1100

    Update bootstrap.php
    Added default image into bootstrap

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -2,3 +2,4 @@
 
 Configure::write('TimthumbBasePath', '/');
 Configure::write('TimthumbCacheDir', TMP . DS . 'timthumb');
+Configure::write('TimthumbDefaultImg', 'img/cake.icon.png'); //Change this to your own image
