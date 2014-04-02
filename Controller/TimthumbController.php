d30d10a99f8a79761a4e3680c4a5b84a8464bac1
Vishal Deep <vishal@logicielsolutions.co.in>
2014-04-02 10:52:54 +0530

    Allowed image action in Auth (if Auth is enabled)

--- Controller/TimthumbController.php
+++ Controller/TimthumbController.php
@@ -3,6 +3,14 @@
 App::uses('TimthumbAppController', 'Timthumb.Controller');
 
 class TimthumbController extends TimthumbAppController {
+    public function beforeFilter() {
+        parent::beforeFilter();
+        
+        if (in_array('Auth', $this->components)) {
+            $this->Auth->allow('image');
+        }
+    }
+
     public function image() {
         $this->autoRender = false;
         $this->layout = null;
